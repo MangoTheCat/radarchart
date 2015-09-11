14811b30378d2bf6758bf63d01fa9200725099e9
dougmet <douglas.j.ashton@gmail.com>
2015-09-11 21:23:21 +0100

    Added scale settings and pass through

--- R/chartJSRadar.R
+++ R/chartJSRadar.R
@@ -2,17 +2,53 @@
 #'
 #' R bindings to the radar plot in the chartJS library.
 #'
+#' @param scores Named list of scores for each axis
+#' @param labs Labels for each axis
+#' @param width Width of output plot
+#' @param height Height of output plot
+#' @param maxScale Max value on each axis
+#' @param scaleStepWidth Spacing between rings on radar
+#' @param scaleStartValue Value at the centre of the radar
+#' @param responsive Logical: whether or not the chart should be responsive and resize when the browser does
+#' @param labelSize Numeric: Point label font size in pixels
+#' @param addDots Logical: Whether to show a dot for each point
+#' @param colMatrix Numeric matrix of rgb colour values
+#' @param polyAlpha Alpha value for the fill of polygons
+#' @param lineAlpha Alpha value for the outlines
+#' @param ... Extra options passed straight to chart.js
+#'
 #' @import htmlwidgets
 #' @import htmltools
 #'
 #' @export
+#' @examples 
+#' labs <- c("Communicator", "Data Wangler", "Programmer", "Technologist",  "Modeller", "Visualizer")
+#'
+#' scores <- list("Rich" = c(9, 7, 4, 5, 3, 7),
+#'  "Andy" = c(7, 6, 6, 2, 6, 9),
+#'  "Aimee" = c(6, 5, 8, 4, 7, 6))
+#'
+#' chartJSRadar(scores=scores, labs=labs)
+#' 
 chartJSRadar <- function(scores, labs, width = NULL, height = NULL,
+                         maxScale=NULL, scaleStepWidth=NULL, scaleStartValue=0,
                          responsive = TRUE, labelSize = 18, addDots = TRUE,
                          colMatrix = diag(3) * 255, polyAlpha = .2,
-                         lineAlpha = .8) {
+                         lineAlpha = .8, ...) {
   
-  opList <- list( responsive = responsive, pointLabelFontSize = labelSize,
-                  pointDot = addDots)
+  # Should we keep variable names consistent from chart.js to R?
+  # Then we can just pass through anything that doesn't need preprocessing
+  
+  # Check for maxScale
+  opScale <- setRadarScale(maxScale, scaleStepWidth, scaleStartValue)
+
+  # Any extra options passed straight through. Names must match existing options
+  # http://www.chartjs.org/docs/#getting-started-global-chart-configuration
+  opPassThrough <- list(...)
+  
+  # Combine scale options, pass through and explicit options
+  opList <- c(list( responsive = responsive, pointLabelFontSize = labelSize,
+                  pointDot = addDots), opScale, opPassThrough)
   
   # forward options using x
   datasets <- lapply(names(scores), function(x) list(label=x))
@@ -45,6 +81,42 @@ chartJSRadar <- function(scores, labs, width = NULL, height = NULL,
   )
 }
 
+#' Autoscale the radar plot
+#'
+#' @param maxScale Numeric length 1. Desired max limit
+#' @param scaleStepWidth Numeric length 1. Spacing between rings
+#' @param scaleStartValue Numeric length 1. Value of the centre
+#'
+#' @return A list containing the scale options for chartjs
+#'
+#' @examples
+#' setRadarScale(15, 3)
+#' setRadarScale(15, 5, 2)
+setRadarScale <- function(maxScale=NULL, scaleStepWidth=NULL, scaleStartValue=0) {
+  
+  if (!is.null(maxScale)) {
+    
+    opScale <- list(scaleOverride=TRUE)
+    
+    # Did they fix the tick points?
+    if (!is.null(scaleStepWidth)) {
+      opScale$scaleStepWidth <- scaleStepWidth
+    } else {
+      if (maxScale-scaleStartValue<=12) {
+        opScale$scaleStepWidth <- 1
+      } else {
+        opScale$scaleStepWidth <- floor((maxScale-scaleStartValue) / 10)
+      }
+    }
+    opScale$scaleSteps <- ceiling((maxScale-scaleStartValue) / 
+                                    opScale$scaleStepWidth)
+    opScale$scaleStartValue <- scaleStartValue
+  } else {
+    opScale <- NULL
+  }
+  
+  opScale
+}
 
 #' @export
 chartJSRadar_html <- function(id, style, class, width, height, ...){
