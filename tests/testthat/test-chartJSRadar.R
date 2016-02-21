e5c5d8c11db0669280d6073bf052f845a41cd582
Douglas Ashton <douglas.j.ashton@gmail.com>
2016-02-21 13:05:08 +0000

    Increasing testing of label handling and data frames

--- tests/testthat/test-chartJSRadar.R
+++ tests/testthat/test-chartJSRadar.R
@@ -1,6 +1,6 @@
 context("chartJSRadar")
 
-test_that("chartJSRadar return object looks about right", {
+test_that("chartJSRadar takes scores and labels", {
 
   labs <- c("Communicator", "Data Wangler", "Programmer", "Technologist",  "Modeller", "Visualizer")
   scores <- list("Rich" = c(9, 7, 4, 5, 3, 7),
@@ -23,5 +23,38 @@ test_that("chartJSRadar return object looks about right", {
                                     "scaleStartValue")))
   
   expect_equal(a$x$data$datasets[[3]]$data, c(6, 5, 8, 4, 7, 6))
+  
+})
+
+test_that("chartJSRadar takes a data frame", {
+  
+  scores <- data.frame("Label"= c("Communicator", "Data Wangler", "Programmer", 
+                            "Technologist",  "Modeller", "Visualizer"),
+                 "Rich" = c(9, 7, 4, 5, 3, 7),
+                 "Andy" = c(7, 6, 6, 2, 6, 9),
+                 "Aimee" = c(6, 5, 8, 4, 7, 6),
+                 "Doug" = c(4,5,8,9,8,2))
+  
+  a <-chartJSRadar(scores=scores, maxScale=10)
+  
+  expect_equal(a$x$data$labels, scores$Label)
+  expect_equal(length(a$x$data$datasets), 4)
+})
+
+test_that("chartJSRadar handles missing labels", {
+  
+  scores <- data.frame("Rich" = c(9, 7, 4, 5, 3, 7),
+                       "Andy" = c(7, 6, 6, 2, 6, 9))
+  
+  # Missing labels and no character column in position 1
+  expect_error(chartJSRadar(scores=scores, maxScale=10))
+  
+  a <-chartJSRadar(scores=scores, labs = NA, maxScale = 10)
+  
+  # NA labels converted to blank
+  expect_equal(a$x$data$labels, c("", "", "", "", "", ""))
+  
+  # If labels are specified they should be the right length
+  expect_error(chartJSRadar(scores=scores, labs = LETTERS[1:3], maxScale=10))
   
 })
