2a8101691693621a2ee4a0e73607fa88d7dd2e06
Douglas Ashton <douglas.j.ashton@gmail.com>
2016-03-03 15:49:00 +0000

    first few options for #6

--- inst/shiny-examples/options/server.R
+++ inst/shiny-examples/options/server.R
@@ -3,7 +3,25 @@ library(radarchart)
 shinyServer(function(input, output) {
   output$radar <- renderChartJSRadar({
     
+    # Convert zero to a NULL
+    maxScaleR <- reactive({
+      if (input$maxScale>0)
+        input$maxScale
+      else
+        NULL
+    })
+    # Convert zero to a NULL
+    scaleStepWidthR <- reactive({
+      if (input$scaleStepWidth>0)
+        input$scaleStepWidth
+      else
+        NULL
+    })
+    
     chartJSRadar(skills[, c("Label", input$selectedPeople)], 
-                 maxScale = 10, showToolTipLabel=TRUE)
+                 maxScale = maxScaleR(),
+                 scaleStepWidth = scaleStepWidthR(),
+                 scaleStartValue = input$scaleStartValue,
+                 showToolTipLabel=TRUE)
   })
 })
--- inst/shiny-examples/options/ui.R
+++ inst/shiny-examples/options/ui.R
@@ -4,7 +4,16 @@ shinyUI(pageWithSidebar(
   headerPanel('Radarchart Shiny Example'),
   sidebarPanel(
     checkboxGroupInput('selectedPeople', 'Who to include', 
-                       names(radarchart::skills)[-1], selected="Rich")
+                       names(radarchart::skills)[-1], selected="Rich"),
+    
+    numericInput("maxScale", "maxScale - 0 for NULL (default)", value = 10, min = 0,
+                 max = NA, step = 1),
+    
+    numericInput("scaleStepWidth", "scaleStepWidth - 0 for NULL (default)", 
+                 value = 0, min = 0, max = NA, step = 1),
+    
+    numericInput("scaleStartValue", "scaleStartValue - 0 is the default", 
+                 value = 0, min = NA, max = NA, step = 1)
   ),
   mainPanel(
     chartJSRadarOutput("radar", width = "450", height = "300"), width = 7
