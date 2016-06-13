dd9fdac7b361ab777cc9d0017cf20005d131a52a
dougmet <douglas.j.ashton@gmail.com>
2016-06-13 16:27:56 +0100

    update to new htmlwidgets factory thing

--- inst/htmlwidgets/chartJSRadar.js
+++ inst/htmlwidgets/chartJSRadar.js
@@ -4,30 +4,29 @@ HTMLWidgets.widget({
 
   type: 'output',
 
-  initialize: function(el, width, height) {
+  factory: function(el, width, height) {
 
     var ctx = el.getContext("2d");
 
     return {
-       ctx: ctx
+
+      renderValue: function(x) {
+
+        // Get the context of the canvas element we want to select
+        var myRadarChart = new Chart(ctx, {
+            type: 'radar',
+            data: x.data,
+            options: x.options
+        });
+
+      },
+
+      resize: function(width, height) {
+
+        // TODO: code to re-render the widget with a new size
+
+      }
+
     };
-  },
-
-  renderValue: function(el, x, instance) {
-
-    
-    // Get the context of the canvas element we want to select
-    var myRadarChart = new Chart(instance.ctx, {
-        type: 'radar',
-        data: x.data,
-        options: x.options
-    });
-
-
-  },
-
-  resize: function(el, width, height, instance) {
-
   }
-
 });
