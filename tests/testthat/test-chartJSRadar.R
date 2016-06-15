78fd9c9f2a9dc3d40b862fde5e09c44baaf3bb9c
Graham Parsons <graham@grahamrp.com>
2016-06-15 09:49:55 +0100

    Make options test less rigid
    Remove the test for an exact options structure and instead test that a few options can be set correctly.

--- tests/testthat/test-chartJSRadar.R
+++ tests/testthat/test-chartJSRadar.R
@@ -51,8 +51,7 @@ test_that("chartJSRadar handles missing labels", {
   expect_error(chartJSRadar(scores = scores, labs = LETTERS[1:3]))
 })
 
-test_that("chartJSRadar object has correct default options", {
-  # Testing the exact structure seems a bit rigid
+test_that("chartJSRadar options can be set correctly", {
   
   labs <- c("Communicator", "Data Wangler", "Programmer", "Technologist",
             "Modeller", "Visualizer")
@@ -61,21 +60,17 @@ test_that("chartJSRadar object has correct default options", {
                  "Aimee" = c(6, 5, 8, 4, 7, 6),
                  "Doug" = c(4, 5, 8, 9, 8, 2))
   
-  a <- chartJSRadar(scores = scores, labs = labs)
+  # Set some non-default options and test object has been set correctly
+  exMaxScale <- 10
+  exResponsive <- FALSE
+  exLabelSize <- 20
   
-  # Define expected options
-  exOptions <- list(responsive = TRUE, 
-                    title = list(
-                      display = FALSE, 
-                      text = NULL),
-                    scale = list(
-                      ticks = list(min = 0),
-                      pointLabels = list(fontSize = 18)),
-                    tooltips = list(
-                      enabled = TRUE,
-                      mode = "label"),
-                    legend = list(
-                      display = TRUE)
-  )
-  expect_equal(a$x$options, exOptions)
+  a <- chartJSRadar(scores = scores, labs = labs, 
+                    maxScale = exMaxScale,
+                    responsive = exResponsive,
+                    labelSize = exLabelSize)
+  
+  expect_equal(a$x$options$scale$ticks$max, exMaxScale)
+  expect_equal(a$x$options$responsive, exResponsive)
+  expect_equal(a$x$options$scale$pointLabels$fontSize, exLabelSize)
 })
