605ab479fc191e1834310d7a9821ab0e85ff6432
Douglas Ashton <douglas.j.ashton@gmail.com>
2016-12-18 22:27:19 +0000

    up to version 2.4.0

--- inst/htmlwidgets/chartJSRadar.js
+++ inst/htmlwidgets/chartJSRadar.js
@@ -12,6 +12,9 @@ HTMLWidgets.widget({
     return {
       renderValue: function(x) {
 
+       if (typeof instance.Chartjs != "undefined") {
+         instance.Chartjs.destroy();
+       }
         // Get the context of the canvas element we want to select
         var myRadarChart = new Chart(ctx, {
             type: 'radar',
