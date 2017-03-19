ca11d436addef477555e5e6126d48493b07eb859
Adeel Khan <AdeelK@gwu.edu>
2017-03-18 21:23:04 -0500

    Update chart rather than redrawing it in Shiny

--- inst/htmlwidgets/chartJSRadar.js
+++ inst/htmlwidgets/chartJSRadar.js
@@ -1,37 +1,58 @@
 HTMLWidgets.widget({
 
   name: 'chartJSRadar',
-
   type: 'output',
 
   factory: function(el, width, height) {
 
     var ctx = el.getContext("2d");
-    var instance = {};
+    var instance = new Chart(ctx, {type: 'radar'});
 
     return {
       renderValue: function(x) {
-        
-        if (typeof instance.Chartjs != "undefined") {
-            instance.Chartjs.destroy();
+
+        if (instance.config.data.datasets.length===0) {
+          // Add data for the first time
+          instance.config.data = x.data;
+          instance.config.options = x.options;
+        } else {
+
+          // Remove extraneous datasets
+          while (instance.config.data.datasets.length > x.data.datasets.length) {
+            instance.config.data.datasets.pop();
+          }
+
+          x.data.datasets.forEach(function(dataset, sid) {
+
+            if (instance.config.data.datasets[sid]) {
+              // Update the existing datasets
+              dataset.data.forEach(function(point, pid) {
+                instance.config.data.datasets[sid].data[pid] = point;
+              })
+              Object.keys(dataset).forEach(function(key) {
+                if(key!=='data') { // don't mutate data key
+                  instance.config.data.datasets[sid][key] = dataset[key];
+                }
+              })
+            } else {
+              // Add new datasets
+              instance.config.data.datasets[sid] = dataset;
+            }
+
+          })
+
         }
-        // Get the context of the canvas element we want to select
-        var myRadarChart = new Chart(ctx, {
-            type: 'radar',
-            data: x.data,
-            options: x.options
-        });
-        
-        instance.Chartjs = myRadarChart;
-  
+
+        // Update the options
+        instance.options = Chart.helpers.configMerge(instance.options, x.options);
+        instance.update();
+
       },
-  
+
       resize: function(width, height) {
-  
-        // TODO: code to re-render the widget with a new size
-  
-      }, 
-      
+        instance.resize();
+      },
+
       instance: instance
     };
   }
